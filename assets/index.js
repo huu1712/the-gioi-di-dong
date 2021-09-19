75bbedafcbf51116915872d3e17cc81d4306ab5e
Chí Thiện <chithien300313@gmail.com>
2021-09-19 13:19:53 +0700

    Add index.js ver 2

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,16 @@
 const itemMenu = document.querySelectorAll('.menu-item')
 
 itemMenu.forEach(function (item) {
+    //cách 1
+    // item.addEventListener("click", function () {
+    //     itemMenu.forEach(function (item) {
+    //         item.classList.remove('active')
+    //     })
+    //     this.classList.add('active')
+    // })
+    //cách 2
     item.addEventListener("click", function () {
-        itemMenu.forEach(function (item) {
-            item.classList.remove('active')
-        })
-        this.classList.add('active')
+        [...item.parentElement.children].forEach(sib => sib.classList.remove('active'))
+        item.classList.add('active')
     })
 })
