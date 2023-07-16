559422f3b0119a694cfeef09c6cc2d020c4c0b7c
huu1712 <89588636+huu1712@users.noreply.github.com>
2023-07-17 00:46:42 +0700

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -2,17 +2,19 @@ const itemMenu = document.querySelectorAll('.menu-item')
 
 itemMenu.forEach(function (item) {
     //cách 1
-    // item.addEventListener("click", function () {
-    //     itemMenu.forEach(function (item) {
-    //         item.classList.remove('active')
-    //     })
-    //     this.classList.add('active')
-    // })
-    //cách 2
     item.addEventListener("click", function () {
-        [...item.parentElement.children].forEach(sib => sib.classList.remove('active'))
-        item.classList.add('active')
+        itemMenu.forEach(function (item) {
+            item.classList.remove('active')
+                            console.log('item ', item)
+
+        })
+        this.classList.add('active')
     })
+    //cách 2
+    // item.addEventListener("click", function () {
+    //     [...item.parentElement.children].forEach(sib => sib.classList.remove('active'))
+    //     item.classList.add('active')
+    // })
 })
 
 //Slider banner
